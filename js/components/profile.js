981edd423b94ec31732f0f7570fac031f7d008a0
princepatrickguru <princepatrickmail@gmail.com>
2022-07-10 11:29:44 -0800

    allpagemenu
    The floating Menu has been added to all page

--- js/components/profile.js
+++ js/components/profile.js
@@ -1,176 +1,80 @@
 // profile of oba
 import { profileOfOba } from "../crest-list.js";
-const crestCardContainer = document.querySelector('.crest-card-container')
-const input = document.getElementById('input')
+const rulingLeadersList = document.getElementById('ruling-leaders-list');
+
+for (let i = 0; i < profileOfOba.length; i++) {
+    const rulerDetail = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const detailscontainer = document.createElement('div');
+    const rulerName = document.createElement('h5');
+    const rulerLifeSpan = document.createElement('p');
+    const reign = document.createElement('h5');
+    const container = document.createElement('div');
+    const reignStart = document.createElement('p');
+    const reignEnd = document.createElement('p');
+    const biography = document.createElement('p');
+ 
+    // appendChild
+    rulingLeadersList.appendChild(rulerDetail);
+    rulerDetail.appendChild(imgContainer);
+    imgContainer.appendChild(image);
+    rulerDetail.appendChild(detailscontainer);
+    detailscontainer.appendChild(rulerName);
+    detailscontainer.appendChild(rulerLifeSpan);
+    detailscontainer.appendChild(reign);
+    detailscontainer.appendChild(container);
+    container.appendChild(reignStart);
+    container.appendChild(reignEnd);
+    detailscontainer.appendChild(biography);
+ 
+    // Add classlist
+    rulerDetail.classList.add('d-flex' , 'col-12' , 'rulerDetails');
+    imgContainer.classList.add('img-container');
+    image.classList.add('img-width');
+    detailscontainer.classList.add('col-7');
+    rulerName.classList.add('text-bold' , 'font-1' , 'rulername');
+    rulerLifeSpan.classList.add('p-1' , 'font-2');
+    reign.classList.add('text-bold' , 'font-2' , 'p-top-1');
+    container.classList.add('d-flex', 'col-12' , 'p-top-1');
+    reignStart.classList.add('font-3' , 'm-r-10');
+    reignEnd.classList.add('font-3');
+    biography.classList.add('d-none');
+ 
+    // setattribute
+    image.setAttribute('src' , profileOfOba[i].image);
+    rulerName.innerText =  profileOfOba[i].name;
+    rulerLifeSpan.innerText =  profileOfOba[i].span;
+    reign.innerText =  profileOfOba[i].reign;
+    reignStart.innerHTML =  profileOfOba[i].start;
+    reignEnd.innerHTML =  profileOfOba[i].end;   
+    biography.innerHTML =  profileOfOba[i]. biography;   
+ }
+ 
+ const rulersDetailsContainers = document.querySelectorAll('.rulerDetails');
+ const imgDisplay = document.getElementById('img-display');
+ const nameDisplay = document.getElementById('name-display');
+ const rulerLifeSpan = document.getElementById('ruler-life-span');
+ const reignStart = document.getElementById('reign-start');
+ const reignEnd = document.getElementById('reign-end');
+ const rulerBiography = document.getElementById('ruler-biography');
 
 
-// display of the profile
-    let mapped = profileOfOba.map(profile=>{
-            crestCardContainer.innerHTML += `
-            <div class="row crest-card mb-10" data-id="${profile.id}" id="${profile.id}">
-            <div class="col-4 no-padding">
-            <img style="padding-right:5px;" width="120px" height="120px" src="${profile.image}" alt="">
-            </div>
-            <div class="col-8">
-                <h5 style="font-size: 1rem;" class="serial">
-                ${profile.name}
-                </h5>
-                <p style="font-size:.75rem;padding-top: 5px;">
-                ${profile.span}
-                </p>
-                <h5 style="font-size:.8rem; padding-top: 12px">
-                    Reign
-                </h5>
-                <div class="row ">
-                    <p class="col-6 " style="font-size: .7rem;">
-                       <b>Start:</b> ${profile.start} 
-                       </p>
-                    <p class="col-6 " style="font-size: .7rem;">
-                        <b>End:</b> ${profile.end}
-                        </p>
-                        </div>
-                        </div>
-        </div>
-        `
-        })
-// input.addEventListener('input',(e)=>{
-//     const keyInputed = input.value
-//     const filterText = keyInputed
-//     const filterData = profileOfOba.filter(data=>data.name.toLowerCase().includes(filterText.toLowerCase()))
-//     // console.log(filterData);
-//     filterData.map(profile=>{
-//         crestCardContainer.innerHTML = `
-//         <div class="row crest-card mb-10" data-id="${profile.id}" id="${profile.id}">
-//             <div class="col-4 no-padding">
-//                 <img style="padding-right:5px;" width="120px" height="120px" src="${profile.image}" alt="">
-//             </div>
-//             <div class="col-8">
-//                 <h5 style="font-size: 1rem;" class="serial">
-//                     ${profile.name}
-//                 </h5>
-//                 <p style="font-size:.75rem;padding-top: 5px;">
-//                     ${profile.span}
-//                 </p>
-//                 <h5 style="font-size:.8rem; padding-top: 12px">
-//                     Reign
-//                 </h5>
-//                 <div class="row ">
-//                     <p class="col-6 " style="font-size: .7rem;">
-//                         <b>Start:</b> ${profile.start} 
-//                     </p>
-//                     <p class="col-6 " style="font-size: .7rem;">
-//                         <b>End:</b> ${profile.end}
-//                     </p>
-//                 </div>
-//             </div>
-//         </div>
-//         `
-//     })
-//     console.log(filterData);
-//     e.preventDefault()
-// })
-
-const crestCard = document.querySelectorAll('.crest-card')
-const clickableCard = crestCard
-const main = document.querySelector('.ginuwa-main-dp')
-const smDp = document.querySelector('.sm-dp-profile')
-
-
-clickableCard.forEach( card=>{
-    card.addEventListener('click', e =>{
-        const crestTarget = e.currentTarget.dataset.id
-        const cardId = e.currentTarget.id
-        const idealCard =  cardId - 1
-        smDp.style.display = "flex"
-        if( crestTarget  ){
-            mainScreenRight(idealCard)
-           smallScreanDp(idealCard)
-        }
-     })
-})
-
-function mainScreenRight(idealCard) {
-    main.innerHTML = `
-            <div class="ginuwa-crest-display" data-id="${profileOfOba[idealCard]}">
-            <div class="main-dp">
-                <img src="${profileOfOba[idealCard].image}" alt="" width="140px" height="140px">
-            </div>
-            <h5>
-                ${profileOfOba[idealCard].name}   
-            </h5>
-            <p style="font-size:.75rem;padding-top: 5px;">
-                ${profileOfOba[idealCard].span}
-            </p>
-            <h5 style="font-size:.8rem; padding-top: 12px">
-                Reign
-            </h5>
-            <div class="row ">
-                <p class="col-6 " style="font-size: .7rem;">
-                   <b>Start:</b> ${profileOfOba[idealCard].start} 
-                </p>
-                <p class="col-6 " style="font-size: .7rem;">
-                    <b>End:</b> ${profileOfOba[idealCard].end}
-                </p>
-                <p style="font-size: .75rem;padding-top: 10px;">
-                    ${profileOfOba[idealCard].note} 
-                </p>
-            </div>
-        </div>
-            `
-}
-function smallScreanDp(idealCard) {
-    smDp.innerHTML = `
-    <div class="sm-dp-container d-flex align-items-center" data-id="${profileOfOba[idealCard]}>
-    <div class="d-flex align-items-center">
-            <div class="fw-bold bk" style="font-size: 1.2rem ;">
-                
-                &#8592
-            </div>
-            <h5  style="font-size: 14.4px; padding-left:10px">
-            ${profileOfOba[idealCard].name}  
-            </h5>
-    </div>
-    <hr>
-    <div class="ginuwa-crest-display">
-        <div class="main-dp">
-            <img src="${profileOfOba[idealCard].image}" alt="" width="140px" height="140px">
-        </div>
-        <h5 style="font-size: 12.8px; padding-top: 12px;">
-            Lifespan      
-        </h5>
-        <p style="font-size:.75rem;padding-top: 5px;">
-        ${profileOfOba[idealCard].span}
-        </p>
-        <h5 style="font-size:.8rem; padding-top: 12px">
-            Reign
-        </h5>
-        <div class="row ">
-            <p class="col-6 " style="font-size: .7rem;">
-               <b>Start:</b> ${profileOfOba[idealCard].start} 
-            </p>
-            <p class="col-6 " style="font-size: .7rem;">
-                <b>End:</b> ${profileOfOba[idealCard].end}
-            </p>
-            <h5 style="font-size: 12.8px; padding-top: 12px;">
-                Family      
-            </h5>
-            <p style="font-size: .75rem; padding-top: 5px;">
-                ${profileOfOba[idealCard].family}
-            </p>
-            <p style="font-size: .75rem;padding-top: 10px;">
-            ${profileOfOba[idealCard].note} 
-            </p>
-    
-        </div>
-    </div>
-    
-    </div>
-    
-    `
-    const btnBk = document.querySelector('.bk')
-    const returnBtn = btnBk 
-    returnBtn.addEventListener('click',()=>{
-        smDp.style.display = "none"
+ rulersDetailsContainers.forEach((rulerDetailsContainer)=> {
+    rulerDetailsContainer.addEventListener('click' , (e)=>{
+       const img = e.currentTarget.firstChild.firstChild;
+       const name = e.currentTarget.firstChild.nextSibling.firstChild;
+       const lifeSpan = e.currentTarget.firstChild.nextSibling.firstChild.nextSibling;
+       const reignstart = e.currentTarget.lastChild.lastChild.previousElementSibling.firstChild;
+       const reignend = e.currentTarget.lastChild.lastChild.previousElementSibling.lastChild;
+       const biography = e.currentTarget.lastChild.lastChild;
+       imgDisplay.setAttribute('src' , img.src);
+       nameDisplay.innerText =  name.textContent;
+       rulerLifeSpan.innerText =  lifeSpan.textContent;
+       reignStart.innerHTML =  reignstart.innerHTML;
+       reignEnd.innerHTML =  reignend.innerHTML;
+       rulerBiography.innerText = biography.textContent;
     })
-}
+ })
 
+ export {rulersDetailsContainers}
