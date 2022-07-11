578dfd456fb991115c954bc1f7e8f40875a15a4e
Esiehor Sophia Eseoghene <esephizzy2@gmail.com>
2022-07-11 14:53:08 +0100

    modified

--- js/components/search.js
+++ js/components/search.js
@@ -1,20 +1,15 @@
 // search filter
-//traditional council search
-import { chiefDetailsContainers } from "../components/profile.js";
-const search = document.getElementById('search-input')
-const names = document.querySelectorAll('.overflow-heading')
-=======
-import { rulersDetailsContainers } from "./profile.js";
-const searchInput = document.getElementById('search-input');
-const rulerName = document.querySelectorAll('.rulerDetails .rulername');
+import { chiefDetailsContainers } from "./profile.js";
+const searchInput = document.querySelector('.input-btn');
+const rulerName = document.querySelectorAll('.overflow-heading');
 
 searchInput.addEventListener('keyup',()=>{
-    for (let i = 0 ; i < rulersDetailsContainers.length ; i++){
+    for (let i = 0 ; i < chiefDetailsContainers.length ; i++){
        if (rulerName[i].innerText.toLowerCase().includes(searchInput.value.toLowerCase())) {
-          rulersDetailsContainers[i].classList.remove('d-none');
+         chiefDetailsContainers[i].classList.remove('d-none');
        }
        else {
-          rulersDetailsContainers[i].classList.add('d-none');
+         chiefDetailsContainers[i].classList.add('d-none');
        }
    }
  })
