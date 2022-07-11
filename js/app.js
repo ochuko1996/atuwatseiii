fe05d7bbb5d13d9005ba8323fc74bfb356a6e7f3
Bigtunzz <104577800+Bigtunzz@users.noreply.github.com>
2022-07-11 17:27:12 +0100

    fixed responsive nav

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const hambugerMenu = document.querySelector('.hambuger-menu')
 const navPositionStyle = document.querySelector('.nav-position-style')
 const arr = [document.querySelector('.hamburger'), document.querySelector('.line')]
+const overlay = document.querySelector('.overlay');
 
 arr.forEach(element => {
     element.addEventListener('click', () =>{
         hambugerMenu.classList.toggle('active')
         navPositionStyle.classList.toggle('active')  
+        overlay.classList.toggle('active')
     })
 });
